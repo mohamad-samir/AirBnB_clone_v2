577b95058f0f8526fd69754a45fbf0401cd0be57
mohamad-samir <mohamad.samir2011@gmail.com>
2023-10-06 21:55:57 +0300

    u

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,57 +1,46 @@
 #!/usr/bin/python3
 """
-Fabric script methods:
-    do_pack: packs web_static/ files into .tgz archive
-    do_deploy: deploys archive to webservers
-    deploy: do_packs && do_deploys
-Usage:
-    fab -f 3-deploy_web_static.py deploy -i my_ssh_private_key -u ubuntu
+Write a Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers, using the function do_deploy
 """
-from fabric.api import local, env, put, run
-from time import strftime
-import os.path
+
+from fabric.api import local
+from datetime import datetime
+from os.path import isfile
+from fabric.api import *
+
+env.user = 'ubuntu'
 env.hosts = ["100.24.235.23", "3.85.141.52"]
 
 
 def do_pack():
-    """generate .tgz archive of web_static/ folder"""
-    timenow = strftime("%Y%M%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        filename = "versions/web_static_{}.tgz".format(timenow)
-        local("tar -cvzf {} web_static/".format(filename))
-        return filename
-    except:
+    """ Generate a .tgz archive from the contents of the web_static folder """
+    time = datetime.now()
+    name = 'web_static_' + str(time.year) + str(time.month) + str(time.day)
+    name = name + str(time.hour) + str(time.minute) + str(time.second) + '.tgz'
+    local('mkdir -p versions')
+    archive = local('tar -cvzf versions/{} web_static'.format(name))
+    if archive.failed:
         return None
+    return 'versions/{}'.format(name)
 
 
 def do_deploy(archive_path):
-    """
-    Deploy archive to web server
-    """
-    if os.path.isfile(archive_path) is False:
+    """ Distribute an archive to the web servers """
+    if not isfile(archive_path):
         return False
-    try:
-        filename = archive_path.split("/")[-1]
-        no_ext = filename.split(".")[0]
-        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
-        symlink = "/data/web_static/current"
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}web_static".format(path_no_ext))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(path_no_ext, symlink))
-        return True
-    except:
-        return False
-
-
-def deploy():
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    success = do_deploy(archive_path)
-    return success
+    put(archive_path, '/tmp/')
+    archive = archive_path.replace('.tgz', '')
+    archive = archive.replace('versions/', '')
+    run('mkdir -p /data/web_static/releases/{}/'.format(archive))
+    run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
+        .format(archive, archive))
+    run('rm /tmp/{}.tgz'.format(archive))
+    run('mv /data/web_static/releases/{}/web_static/* '.format(archive) +
+        '/data/web_static/releases/{}/'.format(archive))
+    run('rm -rf /data/web_static/releases/{}/web_static'.format(archive))
+    run('rm -rf /data/web_static/current')
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+        .format(archive))
+    print('New version deployed!')
+    return True
