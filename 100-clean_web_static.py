3a81445ef99e0ddaefa2579d3ddfa32f48ca0c01
mohamad-samir <mohamad.samir2011@gmail.com>
2023-10-07 17:19:03 +0300

    u

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 from fabric.api import *
 
 # List of hosts to connect to
-env.hosts = ['54.160.85.72', '35.175.132.106']
+env.hosts = ["100.24.235.23", "3.85.141.52"]
 
 def do_clean(number=0):
     """Delete out-of-date archives.
